59c680f4bebeb11f517d99cd94e4fe5123e3a849
alli <dmsql2670@gmail.com>
2021-10-27 22:49:51 +0900

    심리테스트 게임 만들기

---
+++ test.js
@@ -0,0 +1,8 @@
+const app= {
+        pages: [],
+        show: new Event('show'),
+        init: function(){
+                app.pages= document.querySelectorAll('.page');
+                    app.pagesfor
+        }
+}
